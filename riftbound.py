9b037ad1627488db0fb8c1cbcad0250890cd57be
Demeron Jones <kyotejones@gmail.com>
2025-08-07 15:42:28 -0700

    Adding Puhover Code

--- riftbound.py
+++ riftbound.py
@@ -1,22 +1,22 @@
 import os
 import requests
 from bs4 import BeautifulSoup
+import http.client, urllib
 
 url = "https://www.gamenerdz.com/riftbound-league-of-legends-tcg-origins-booster-box-sealed-case-preorder"
-#url = "https://www.gamenerdz.com/magic-the-gathering-innistrad-crimson-vow-set-booster-box"
 strPushoverToken = os.environ["PUSHOVER_TOKEN"]
 strPushoverUser = os.environ["PUSHOVER_USER"]
 strMessage = "Riftbound is available for order"
 
-def send_pushover_notification(token, user, message):
-    url = "https://api.pushover.net/1/messages.json"
-    data = {
-        "token": token,
-        "user": user,
-        "message": message
-    }
-    response = requests.post(url, data=data)
-    return response.json()
+def send_pushover_notification():
+    objConn = http.client.HTTPSConnection("api.pushover.net:443")
+    objConn.request("POST", "/1/messages.json",
+    urllib.parse.urlencode({
+        "token": strPushoverToken,
+        "user": strPushoverUser,
+        "message": strMessage,
+    }), { "Content-type": "application/x-www-form-urlencoded" })
+    objConn.getresponse()
 
 response = requests.get(url)
 soup = BeautifulSoup(response.text, "html.parser")
@@ -29,7 +29,7 @@ print(availability)
 
 if availability == None:
     print("Product is available for order")
-    send_pushover_notification(strPushoverToken, strPushoverUser, strMessage)
+    send_pushover_notification()
     print("Pushover notification sent")
 else:
     print("Produict is not available for order")
