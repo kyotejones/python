f2994e0c0932c41d0a23de447ce1e70d2e6e3f7b
Demeron Jones <kyotejones@gmail.com>
2025-08-29 10:59:29 -0700

    save to local file

--- woot.com.py
+++ woot.com.py
@@ -1,14 +1,19 @@
+import time
 import requests
-import json
 import re
 import http.client
 import urllib
 import os
+import json
+
+str_woot_api_key = os.environ["WOOT_API_KEY"]
+str_pushover_token = os.environ["PUSHOVER_WOOT_TOKEN"]
+str_pushover_user = os.environ["PUSHOVER_USER"]
 
 URL = "https://developer.woot.com/feed/All"
 HEADERS = {
     "Accept": "application/json",
-    "x-api-key": os.environ["WOOT_API_KEY"],  # Ensure you have set this environment variable
+    "x-api-key": str_woot_api_key,  # Ensure you have set this environment variable
 }
 
 def main():
@@ -16,11 +21,12 @@ def main():
     response.raise_for_status()
     data = response.json()
 
+    # Troubleshooting
     #print(json.dumps(data, indent=2))
 
     # Search for regex "magic.*gathering" in the results
-    pattern = re.compile(r"magic.*gathering", re.IGNORECASE)
-    #pattern = re.compile(r"carbon.*monoxide", re.IGNORECASE)
+    #pattern = re.compile(r"magic.*gathering", re.IGNORECASE)
+    pattern = re.compile(r"External.*DVD", re.IGNORECASE)
     matches = []
     str_message = ""
 
@@ -35,19 +41,20 @@ def main():
             print(f"- {match.get('Title')} ({match.get('Url')})")
             str_message += f"- {match.get('Title')} ({match.get('Url')})\n"
         send_pushover_notification(str_message)
+
+        # store data in a file under /tmp/
+        with open(f"/tmp/woot_results_{int(time.time())}.json", "w") as f:
+            json.dump(data, f)
     else:
         print('No results found matching regex "magic.*gathering".')
 
 def send_pushover_notification(str_message):
 
-    strPushoverToken = os.environ["PUSHOVER_WOOT_TOKEN"]
-    strPushoverUser = os.environ["PUSHOVER_USER"]
-
     objConn = http.client.HTTPSConnection("api.pushover.net:443")
     objConn.request("POST", "/1/messages.json",
     urllib.parse.urlencode({
-        "token": strPushoverToken,
-        "user": strPushoverUser,
+        "token": str_pushover_token,
+        "user": str_pushover_user,
         "message": str_message,
     }), { "Content-type": "application/x-www-form-urlencoded" })
     objConn.getresponse()
